85ad6e18bb2a01a3194f0c083ffde0f0eb9b73d4
Flaxtrux <caruntuflavius9@gmail.com>
2026-06-04 13:34:08 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-require('dotenv').config();
 
 if (!process.env.TOKEN) {
     console.error('❌ TOKEN no encontrado, revisa el .env');
